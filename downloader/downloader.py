0362f6775a6df5e23a1aa4e573e3cb6593396e53
Carpi <colonnamanu98@gmail.com>
2023-12-21 12:33:34 +0100

    Refactor generate_playlist function to remove unnecessary code

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -6,13 +6,12 @@ def generate_playlist(playlist_url):
     annotations = []
     playlist = Playlist(playlist_url)
 
-    for i, url in enumerate(playlist):
+    for url in playlist:
         annotations.append(
             {
                 # "id": i,
                 "url": url,
             }
         )
-        # video = url.streams.first().download()
     logger.info(f"Find {len(annotations)} videos")
     return annotations
