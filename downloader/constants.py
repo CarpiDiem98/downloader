1d2f711224958bbfe7dd4415abd482e15beb7683
Carpi <colonnamanu98@gmail.com>
2023-12-21 20:40:31 +0100

    Add constants file for downloader module

---
+++ downloader/constants.py
@@ -0,0 +1,59 @@
+WELCOME = """
+     _______________________________
+    |                               |
+    |  Starting Downloader v0.0.1   |
+    |  For help, use --help module  |
+    |  by: Carpi                    |
+    |_______________________________|
+"""
+
+END = """
+     _______________________________
+    |                               |
+    |  Downloader Finished          |
+    |  Thank you for using our tool |
+    |  by: Carpi                    |
+    |_______________________________|
+"""
+
+ALL_DOWNLOADED = """
+     _______________________________
+    |                               |
+    |  All videos already downloaded|
+    |  Nothing to do =)             |
+    |_______________________________|
+"""
+
+NOT_JSON_FILE = """
+     _______________________________
+    |                               |
+    |  Json file not found.         |
+    |  Please provide a valid file  |
+    |  or check the --help module   |
+    |  for more information.        |
+    |_______________________________|
+"""
+
+NO_DOWNLOAD_FLAG = """
+     _______________________________
+    |                               |
+    |  No download flag provided.   |
+    |  All processes finished.      |
+    |  For help, use --help module  |
+    |_______________________________|
+"""
+
+NO_URL_PROVIDED = """
+     _______________________________
+    |                               |
+    |  No URL provided.             |
+    |  Checking for JSON file...    |
+    |_______________________________|
+"""
+JSON_FOUND_MERGING = """
+     _______________________________
+    |                               |
+    |  Json file found.             |
+    |  Merging lists...             |
+    |_______________________________|
+"""
