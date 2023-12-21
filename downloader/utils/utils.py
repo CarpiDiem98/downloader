9bf0f6be1ec302049d59bf294a9d84c4e6a3b778
Carpi <colonnamanu98@gmail.com>
2023-12-21 15:01:23 +0100

    Add make_directory function to create directories if they do not exist

--- downloader/utils/utils.py
+++ downloader/utils/utils.py
@@ -1,5 +1,6 @@
 import json
 from downloader.logger.logger import logger
+import os
 
 
 def create_json(file_json, annotations):
@@ -23,3 +24,11 @@ def read_json(file_json):
     with open(file_json, "r") as infile:
         data = json.load(infile)
     return data
+
+
+def make_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        logger.info(f"{directory} created")
+    else:
+        logger.info(f"{directory} folder already exists")
