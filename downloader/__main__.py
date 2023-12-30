75ea686f2db5ff44da3b74736022ed3ac337c9ee
Carpi <colonnamanu98@gmail.com>
2023-12-30 18:00:15 +0100

    Add download and transcribe functionality

--- downloader/__main__.py
+++ downloader/__main__.py
@@ -1,16 +1,19 @@
 from downloader.parser.args import init_parser
 from downloader.annotations import generate_from_playlist, generate_from_video
+from downloader.transcribe.transcribe import transcribe
 from downloader.utils.json_crud import create_json, read_json
 from downloader.utils.utils import (
     compare_and_merge_lists,
     compare_list_to_folder,
-    make_directory,
+    make_download_directory,
+    make_transcript_directory,
 )
-from downloader.downloader.downloader import Downloader
+from downloader.downloader.downloader import download_yt
 from downloader.logger.logger import logger
 from pytube import Playlist, YouTube
 import os
 from downloader.constants import (
+    DOWNLOADING_VIDEOS_MESSAGE,
     WELCOME,
     END,
     ALL_DOWNLOADED,
@@ -19,18 +22,24 @@ from downloader.constants import (
     JSON_FOUND_MERGING,
     NO_URL_PROVIDED,
     NO_DOWNLOAD_FLAG,
+    TRANSCRIPT_FLAG,
+    JSON_FOUND,
 )
 
 
 if __name__ == "__main__":
     args = init_parser()
     logger.info(WELCOME)
+
     if args.url:
         logger.info(f"URL provided: {args.url}")
         if args.type == "playlist":
             playlist = generate_from_playlist(Playlist(args.url))
-        else:
+        elif args.type == "video":
             playlist = generate_from_video(YouTube(args.url))
+        else:
+            logger.info("No type provided")
+            exit()
 
         if os.path.isfile(args.file_json):
             logger.info(JSON_FOUND_MERGING)
@@ -43,24 +52,39 @@ if __name__ == "__main__":
     else:
         logger.info(NO_URL_PROVIDED)
         if os.path.isfile(args.file_json):
+            logger.info(JSON_FOUND)
             playlist = read_json(args.file_json)
-            playlist = compare_and_merge_lists(read_json(args.file_json), playlist)
-            create_json(args.file_json, playlist)
         else:
             logger.info(NOT_JSON_FILE)
 
     if args.download:
-        missing_videos = compare_list_to_folder(
-            read_json(args.file_json), args.output_path
-        )
+        if args.output_path:
+            make_download_directory(args.output_path)
+            missing_videos = compare_list_to_folder(
+                read_json(args.file_json),
+                args.output_path,
+            )
+        else:
+            missing_videos = read_json(args.file_json)
         if len(missing_videos) != 0:
-            logger.info(f"Downloading {len(missing_videos)} videos")
-            make_directory(args.output_path)
+            logger.info(DOWNLOADING_VIDEOS_MESSAGE.format(len(missing_videos)))
+            make_download_directory(args.output_path)
             for video in missing_videos:
-                Downloader(video["url"], args.output_path).download_yt()
+                download_yt(video["url"], args.output_path + "/audio/")
         else:
             logger.info(ALL_DOWNLOADED)
     else:
         logger.info(NO_DOWNLOAD_FLAG)
 
+    if args.transcribe:
+        logger.info(TRANSCRIPT_FLAG)
+        make_transcript_directory(args.output_path)
+        for audio in os.listdir(args.output_path + "/audio"):
+            transcribe(
+                args.model_dimension,
+                os.path.join(args.output_path + "/audio", audio),
+                args.output_path + "/transcripts",
+            )
+
     logger.info(END)
+    exit()
