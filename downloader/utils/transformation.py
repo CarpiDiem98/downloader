7f45bf33e376cc43e3a511a57bddd5b8af6a96c9
Carpi <colonnamanu98@gmail.com>
2024-01-15 18:36:23 +0100

    Add crop_left_side function

---
+++ downloader/utils/transformation.py
@@ -0,0 +1,12 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.fx.all import crop
+
+
+def crop_left_side(input_file, output_file):
+    video_clip = VideoFileClip(input_file)
+    width, height = video_clip.size
+    left_width = width // 2 - 120
+    cropped_clip = crop(video_clip, x1=0, y1=0, x2=left_width, y2=height)
+    cropped_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+    video_clip.close()
+    cropped_clip.close()
