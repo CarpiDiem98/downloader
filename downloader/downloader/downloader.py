88eb10d397c6ddbdc469df09b6e310edf4941154
Carpi <colonnamanu98@gmail.com>
2024-01-16 20:07:31 +0100

    Refactor downloader module and add classes for audio and video downloading

--- downloader/downloader/downloader.py
+++ downloader/downloader/downloader.py
@@ -1,58 +1,68 @@
 from downloader.logger.logger import logger
 from downloader.utils.utils import std_str
+from concurrent.futures import ThreadPoolExecutor
 import yt_dlp
-import ffmpeg
 import os
 
 
-def download_audio(url, output_path):
-    ydl_opts = {
-        "format": "bestaudio",
-        "outtmpl": output_path + "%(title)s.%(ext)s",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            },
-        ],
-        "quiet": True,
-    }
+class DownloaderAudio:
+    def __init__(self, output_path):
+        self.output_path = output_path
 
-    if isinstance(url, list):
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(url)
-    else:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url)
-            ydl.download([url])
+    def download_yt_audio(self, urls):
+        try:
+            self._download_audio(urls, self.output_path)
+        except:
+            logger.error(f"Error downloading: {urls}")
+        _rename_files(self.output_path)
 
-
-def download_video(url, output_path):
-    ydl_opts = {
-        "format": "bestvideo",
-        "outtmpl": output_path + "%(title)s.%(ext)s",
-        "postprocessors": [
-            {
-                "key": "FFmpegVideoConvertor",
-                "preferedformat": "mp4",
-            }
-        ],
-        "quiet": True,
-    }
-    if isinstance(url, list):
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(url)
-    else:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url)
-            ydl.download([url])
-
-    vid = os.path.join(output_path, std_str(info_dict["title"]))
-    return vid
+    def _download_audio(self, urls):
+        ydl_opts = {
+            "format": "bestaudio",
+            "outtmpl": self.output_path + "%(title)s.%(ext)s",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                },
+            ],
+            "quiet": True,
+        }
+        _download_url(urls, ydl_opts)
 
 
-def rename_files(output_path):
+class DownloaderVideo:
+    def __init__(self, output_path):
+        self.output_path = output_path
+
+    def download_videos(self, urls, max_workers=5):
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            return list(executor.map(self._download_video, urls))
+
+    def download_yt_video(self, url):
+        try:
+            self._download_video(url, self.output_path)
+        except:
+            logger.error(f"Error downloading: {url}")
+        _rename_files(self.output_path)
+
+    def _download_video(self, url):
+        ydl_opts = {
+            "format": "bestvideo",
+            "outtmpl": self.output_path + "%(title)s.%(ext)s",
+            "postprocessors": [
+                {
+                    "key": "FFmpegVideoConvertor",
+                    "preferedformat": "mp4",
+                }
+            ],
+            "quiet": True,
+        }
+        _download_url(url, ydl_opts)
+
+
+def _rename_files(output_path):
     files = os.listdir(output_path)
     for file in files:
         old_file_path = os.path.join(output_path, file)
@@ -60,39 +70,6 @@ def rename_files(output_path):
         os.rename(old_file_path, new_file_path)
 
 
-def download_yt_audio(url, output_path):
-    try:
-        download_audio(url, output_path)
-    except:
-        logger.error(f"Error downloading: {url}")
-    rename_files(output_path)
-
-
-def download_yt_video(url, output_path):
-    try:
-        download_video(url, output_path)
-    except:
-        logger.error(f"Error downloading: {url}")
-    rename_files(output_path)
-
-
-def combine_aud_vid(aud, vid, output_path):
-    try:
-        input_video = ffmpeg.input(vid)
-        input_audio = ffmpeg.input(aud)
-
-        output = ffmpeg.output(
-            input_video.video,
-            input_audio.audio,
-            output_path,
-            vcodec="copy",
-            acodec="libmp3lame",
-        )
-        output.run()
-        output.close()
-
-        os.remove(aud)
-        os.remove(vid)
-
-    except ffmpeg.Error as e:
-        logger.error(e)
+def _download_url(url, ydl_opts):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
